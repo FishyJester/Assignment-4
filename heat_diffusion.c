08144d18e41fb4ec5e9073dd5f58b4f75773bcc0
FishyJester <vetekorn@gmail.com>
2019-02-19 16:53:16 +0100

    Update heat_diffusion.c
    cleaning

--- heat_diffusion.c
+++ heat_diffusion.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#define CL_USE_DEPRECATED_OPENCL_1_2_APIS
-#include <CL/cl.h>	//is in /usr/include as probably expected
-			//libOpenCL.so is in /usr/lib64, does this need special handling?
+#define CL_USE_DEPRECATED_OPENCL_1_2_APIS // To avoid warnings.
+#include <CL/cl.h>
 
 
 int main(int argc, char *argv[]){
 	
-	//Get Platform, stolen from lecture slide
+	//Get Platform
 	cl_int error;
 	cl_platform_id platform_id;
 	cl_uint nmb_platforms;
@@ -17,7 +16,7 @@ int main(int argc, char *argv[]){
 		return 1;
 	}
 	
-	//Get device, stolen likewise
+	//Get device
 	cl_device_id device_id;
 	cl_uint nmb_devices;
 	if ( clGetDeviceIDs (platform_id, CL_DEVICE_TYPE_GPU, 1, &device_id, &nmb_devices) != CL_SUCCESS ) {
@@ -25,7 +24,7 @@ int main(int argc, char *argv[]){
 		return 1;
 	}
 
-	//Get context, by thievery
+	//Get context
 	cl_context context;
 	cl_context_properties properties[] =
 	{
@@ -41,7 +40,7 @@ int main(int argc, char *argv[]){
 
 
 	
-	//Creating command queue, copied
+	//Creating command queue
 	cl_command_queue command_queue;
 	command_queue = clCreateCommandQueue ( context, device_id, 0, &error );
 	if ( error != CL_SUCCESS ) {
@@ -56,7 +55,7 @@ int main(int argc, char *argv[]){
 	float central;	//central value at start
 	float c;	//diffusion constant
 
-	//TODO: read arguments:
+	//parse command line arguments arguments:
 	//first two are width, height in that order
 	//-i gives central
 	//-d gives c
@@ -68,8 +67,8 @@ int main(int argc, char *argv[]){
 	central = atof(&argv[3][2]);
 	c = atof(&argv[4][2]);
 	iter = atol(&argv[5][2]);
-//	printf("Width %d\nHeight %d\nCentral %f\nConstant %f\nIterations %d\n", width, height, central, c, iter);
 
+// 	Test:
 //	central = 1e10;
 //	c = 0.02;
 //	width = 10000;
@@ -79,7 +78,6 @@ int main(int argc, char *argv[]){
 	
 	//Building OpenCL program
 	char * run_heat_diff_src;
-	//TODO: Read program from run_heat_diff.cl
 	FILE * file = fopen ("run_heat_diff.cl", "r");
 	fseek ( file, 0, SEEK_END );
 	size_t fS = ftell ( file );
@@ -154,16 +152,12 @@ int main(int argc, char *argv[]){
 	
 
 	// Initialize
-	float * heatmap = (float*)calloc (size, sizeof(float)); //[width+2][height+2] for padding with zeroes? [width*height] for 1-dimensional? malloc?
-	heatmap[(width/2)    *height + height/2]     += central/4;	//does this work? Is it the right way to do it?
+	float * heatmap = (float*)calloc (size, sizeof(float)); 
+	heatmap[(width/2)    *height + height/2]     += central/4;	
 	heatmap[((width-1)/2)*height + height/2]     += central/4;
 	heatmap[(width/2)    *height + (height-1)/2] += central/4;
 	heatmap[((width-1)/2)*height + (height-1)/2] += central/4;
 
-
-	//Enqueue write buffer = ?
-	//error = clEnqueueWriteBuffer ( command_queue, input_buffer_a, CL_TRUE, 0, ix_m*sizeof(float), a, 0, NULL, NULL);
-	//input_buffer_a = clCreateBuffer ( context, CL_MEM_READ_ONLY, sizeof(float) * ix_m, NULL, &error);
 	
 	error = clEnqueueWriteBuffer ( command_queue, heatmap_buffer_0, CL_TRUE, 0, sizeof(float) * size, heatmap, 0, NULL, NULL );
 	if ( error != CL_SUCCESS ) {
@@ -185,7 +179,7 @@ int main(int argc, char *argv[]){
 		fprintf ( stderr, "Cannot enqueue write buffer\n" );
 		return 1;
 	}
-	error = clSetKernelArg ( kernel_0, 0, sizeof(cl_mem), &heatmap_buffer_0 );	//TODO: Error handling?
+	error = clSetKernelArg ( kernel_0, 0, sizeof(cl_mem), &heatmap_buffer_0 );	//TODO: Error handling
 	if ( error != CL_SUCCESS ) {
 		fprintf ( stderr, "Cannot set args\n" );
 		return 1;
@@ -211,7 +205,7 @@ int main(int argc, char *argv[]){
 		return 1;
 	}
 
-	error = clSetKernelArg ( kernel_1, 0, sizeof(cl_mem), &heatmap_buffer_0 );	//TODO: Error handling?
+	error = clSetKernelArg ( kernel_1, 0, sizeof(cl_mem), &heatmap_buffer_0 );	//TODO: Error handling
 	if ( error != CL_SUCCESS ) {
 		fprintf ( stderr, "Cannot set args\n" );
 		return 1;
@@ -240,13 +234,12 @@ int main(int argc, char *argv[]){
 	const size_t offset[2] = {1,1};
 	const size_t work_size[2] = {height-2, width-2};
 
-//ACTUALLY RUN THE PROGRAM!!!!
+//ACTUALLY RUN THE PROGRAM
 	for ( size_t ix=0; ix < iter-1; ix += 2 ) {	
 		clEnqueueNDRangeKernel ( command_queue, kernel_0, 2, offset, work_size, NULL, 0, NULL, NULL );
 		clEnqueueNDRangeKernel ( command_queue, kernel_1, 2, offset, work_size, NULL, 0, NULL, NULL );
-//		clFlush ( command_queue );
-//		clFinish ( command_queue );
 	}
+	
 	float * heatmap_out = malloc ( size*sizeof(float) );
 	if ( iter % 2 == 1 ) {
 		clEnqueueNDRangeKernel ( command_queue, kernel_0, 2, offset, work_size, NULL, 0, NULL, NULL );
@@ -277,6 +270,7 @@ int main(int argc, char *argv[]){
 	clReleaseCommandQueue ( command_queue );
 	clReleaseContext(context);
 
+	//Printing diffusion map, OBS: only for small maps, i.e 5*5, etc.
 	float total_heat = 0;
 	float abs_diff_m = 0;
 	for ( size_t ix=0; ix < height; ++ix ) {
